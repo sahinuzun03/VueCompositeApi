0f91f01d028e7def76f7b4e3c3b724eaa3b2fbb6
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-05 22:17:04 +0300

    html taglerini ref ile kullanımı

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="home">
     <p>{{ name }} - {{ job }}</p>
+
+    <p ref="info">Deneme</p>
+    <!-- yukarıda ki p tagi komple benim info.value'nun değeri olmuş oldu. -->
     <button @click="clicked">Tıkla</button>
   </div>
 </template>
 
 <script>
 //Setup içerisinde çağırmak için kullandık.
-import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+//ref referans bir değer oluşturmak için kullanıyoruz
 
 export default {
   name: 'HomeView',
@@ -17,13 +22,16 @@ export default {
     let name="Şahin"
     let job = "Software Developer"
 
+    const info = ref(null);//bir fonksiyon ilk değer olarak null veriyorum
+
+    //Bu info'yu yukarıda bir yerde kullanacaksam bunu return etmem gerekiyor eğer yukarıda bir yerde kullanmayacaksam eğer return etmeme gerek yoktur.
+
     const clicked = () => {
-      console.log("Butona Tıklandı");
+      // console.log("Butona Tıklandı");
+
+      console.log(info , info.value);
     }
-
-
-    return {name,job,clicked}; 
-    //Burada tanımlı dataları yukarıda kullanabilmek için return etmem gerekmektedir.Bunlar metot olsa dahi return etmem gerekmektedir.
+    return {name,job,clicked,info}; 
     }
   }
 </script>
