73dea8840d355e8de06c94504ff650ea01680c10
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-07 09:49:42 +0300

    wathc / watchEffect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,14 +9,14 @@
 
 <script>
 //Setup içerisinde çağırmak için kullandık.
-import { ref,computed } from 'vue';
+import { ref,computed,watch,watchEffect } from 'vue';
 
 
 //ref referans bir değer oluşturmak için kullanıyoruz
 
 export default {
   name: 'HomeView',
-  components: {},
+
   setup(){
     const days = ref(["Pazartesi","Sali","Çarşamba","Perşembe","Cuma"]);
 
@@ -24,6 +24,17 @@ export default {
     const finded = computed(() => {
       return days.value.filter((day) => day.includes(search.value))
     })
+
+    //search değiştikçe watch çalışmaya başladı
+    const watchStop = watch(search,() => {
+      console.log("watch çalıştı");
+    })
+
+    //Wathceffec hiçbir şey olmadanda bir kere çalıştı.
+    const watchEffectStop = watchEffect(() => {
+      console.log("watcheffect çalıştı",days.value);
+      console.log("watcheffect çalıştı",search.value);
+    });
 
     return {days,search,finded}
     }
