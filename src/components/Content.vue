5b36b604a9e05f586effc38ee9c0c96158b9254a
Sahin Uzun <sahinuzun03@gmail.com>
2023-02-07 11:25:40 +0300

    fetch kullanımı

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,16 +3,35 @@
         <div v-for="content in contents" :key="content.id">
             {{ content.title }}
         </div>
+        <div v-if="err">
+            {{ err }}
+        </div>
     </div>
 </template>
 
 <script>
 import { log } from 'console';
-
+import { ref } from 'vue';
 export default {
     props:['contents'],
-    setup(props){
-        console.log(props.contents)
+    setup(){
+        const contents = ref([]);
+        const err = ref(null);
+        const getContents = async () => {
+            let data = await fetch('https://jsonplaceholder.typicode.com/posts');
+            contents.value = await data.json();
+
+            try {
+                if(!data.ok){
+                throw new Error('Verilere Erişilemedi!!!"')
+            }
+            } catch (error) {
+                err.value = error.message;
+            }
+        }
+
+        getContents();
+        return {contents,err}
     }
 }
 </script>
